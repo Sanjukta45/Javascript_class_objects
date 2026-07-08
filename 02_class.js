2d8ba6277fa85ba76b08e6613412ce9e68f645ea
Sanjukta <sanjuktagiri2021@gmail.com>
2026-07-08 06:28:05 +0000

    Classes in javascript

---
+++ 02_class.js
@@ -0,0 +1,19 @@
+class Employee {
+    constructor (name,empId){
+        this.name = name;
+        this.empId = empId;
+        console.log("New Employee Created");
+    }
+
+    getName(){
+        console.log("Name is : " + this.name);
+    }
+
+    getEmpId(){
+        console.log("EmpId is : " + this.empId);
+    }
+}
+
+const sanjukta = new Employee("Sanjukta Giri", 2779989);
+sanjukta.getName();
+sanjukta.getEmpId();
