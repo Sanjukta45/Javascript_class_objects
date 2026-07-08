cb3377a048a43f79e681dda94e3a7d34eeb05b01
Sanjukta <sanjuktagiri2021@gmail.com>
2026-07-08 05:56:48 +0000

    Objects in js

---
+++ 01_objects.js
@@ -0,0 +1,29 @@
+const employee = {
+    name : "Sanjukta Giri",
+    empId : 2779989,
+    location : "Kolkata",
+    employeeDetails() {
+        console.log("Employee Name : " + this.name);
+        console.log("Employee Id : " + this.empId);
+        console.log("Location : "+ this.location);
+    }
+}
+
+// console.log(employee);
+// console.log(employee.name);
+// employee.employeeDetails.call(employee);
+
+
+const karan = {
+    name : "Karan",
+    empId : 2779988,
+    location : "Kolkata",
+    employeeDetails(){
+        console.log(this.name);
+    }
+}
+
+karan.__proto__ = employee;
+
+console.log(karan);
+karan.employeeDetails();
